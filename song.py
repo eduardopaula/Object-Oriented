3a8e153d421daa2123b28665baf6f1133652253c
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-03-18 13:23:14 -0300

    Aula 343

---
+++ song.py
@@ -0,0 +1,22 @@
+class Song:
+    """Class to represent a song
+
+    Attributes:
+        title (str): The title of the song
+        artist (Artist): An artist object representing the songs creator.
+        duration (int): The duration of the song in secongs. May be zero
+    """
+
+    def __init__(self, title, artist, duration=0):
+        """ Song init method
+
+        Args:
+            title (str): Initialises the 'title' attribute
+            artist (Artist): At artist object representing the song's creator
+            duration (Optional[int]): Initial value for the 'duration' attribute.
+                Will default to zero if not specified.
+        """
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+
