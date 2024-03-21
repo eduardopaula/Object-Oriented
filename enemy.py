cf53e73dd6f07e2be29a7bb8016ea8720531baf3
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-03-21 15:07:42 -0300

    Aula 358

--- enemy.py
+++ enemy.py
@@ -5,6 +5,7 @@ class Enemy(object):
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -13,10 +14,27 @@ class Enemy(object):
             print("I took {} points damage and have {} left".format(damage, self.hit_points))
         else:
             self.lives -= 1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+            else:
+                print("{0.name} is dead".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
 
 
 class Troll(Enemy):
-    pass
+
+    def __init__(self, name):
+        # super(Troll, self).__init__(name=name, lives=1, hit_points=23)
+        super().__init__(name=name, lives=1, hit_points=23)
+
+    def grunt(self):
+        print("Me {0.name}. {0.name} stomp you".format(self))
+
+
+class Vampyre(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
