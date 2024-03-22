b26a7793db8acc90bb19178c96aafa03c2309f5f
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-03-22 10:21:45 -0300

    Aula 360

--- enemy.py
+++ enemy.py
@@ -18,13 +18,13 @@ class Enemy(object):
         else:
             self._lives -= 1
             if self._lives > 0:
-                print("{0.name} lost a life".format(self))
+                print("{0._name} lost a life".format(self))
             else:
-                print("{0.name} is dead".format(self))
+                print("{0._name} is dead".format(self))
                 self._alive = False
 
     def __str__(self):
-        return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
+        return "Name: {0._name}, Lives: {0._lives}, Hit points: {0._hit_points}".format(self)
 
 
 class Troll(Enemy):
@@ -34,7 +34,7 @@ class Troll(Enemy):
         super().__init__(name=name, lives=1, hit_points=23)
 
     def grunt(self):
-        print("Me {0.name}. {0.name} stomp you".format(self))
+        print("Me {0._name}. {0._name} stomp you".format(self))
 
 
 class Vampyre(Enemy):
@@ -44,7 +44,7 @@ class Vampyre(Enemy):
 
     def dodges(self):
         if random.randint(1, 3) == 3:
-            print("***** {0.name} dodges *****".format(self))
+            print("***** {0._name} dodges *****".format(self))
             return True
         else:
             return False
@@ -54,6 +54,14 @@ class Vampyre(Enemy):
             super().take_damage(damage=damage)
 
 
+class VampyreKing(Vampyre):
+
+    def __init__(self, name):
+        super().__init__(name)
+        self._hit_points = 140
+
+    def take_damage(self, damage):
+        super().take_damage(damage // 4)
 
 
 
